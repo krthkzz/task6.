e5b734c8c732ec063a1b7de40c8b452e973119ef
sai karthik <sribsk1729@gmail.com>
2023-10-14 23:49:24 +0530

    first commit

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/krthkzz'
+      className='link footer__link'
+    >
+      Created By Karthik
+    </a>
+  </footer>
+)
+
+export default Footer
